ca70308ec3478fcbff8e2f090873e0672afef24f
Garry Nathanael <garry.nathanael@gmail.com>
2024-10-17 09:17:32 +0700

    Hero and About Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,19 @@
 module.exports = {
   content: ['index.html'],
   theme: {
+    container:{
+      center:true,
+      padding:'16px',
+    },
     extend: {
       backgroundImage: {
         'blue-pink-purple': 'linear-gradient(to right, #3b82f6, #ec4899, #9333ea)',
       },
       colors: {
         dark : '#0f172a',
+      },
+      screens:{
+        '2xl' : '1320px',
       }
     },
   },
